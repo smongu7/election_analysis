bb2a435662ce1f8c5e4c939e348fb5861b8d580e
smong <smongu7@gmail.com>
2022-01-14 13:24:09 -0600

    Adding PyPoll.py file

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+# The data we need to retrieve
+# 1. the total number of votes cast
+# 2. a complete list of candidates
+# 3. percentage of votes each candidate won
+# 4. total number of votes each candidate won
+# 5. winner of election
